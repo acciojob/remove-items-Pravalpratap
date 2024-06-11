cab49210fb4676240cfd30b67c01553229ebd0e5
Praval Pratap Singh <113175059+Pravalpratap@users.noreply.github.com>
2024-06-11 21:21:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-//your JS code here. If required.
+ document.addEventListener("DOMContentLoaded", function() {
+            let selectElement = document.getElementById("colorSelect");
+            let btn = document.getElementById("btn");
+
+           btn.addEventListener("click", function(event) {
+                let selectedIndex = selectElement.selectedIndex;
+                if (selectedIndex !== -1) {
+                    selectElement.remove(selectedIndex);
+                }
+            });
+        });
